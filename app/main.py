cbbd4fa5ee2a88227387da8af815df5eab39315f
serii burduja <seriiburduja@gmail.com>
2025-01-28 18:33:27 +0200

    feat: docker-python-mongo

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 from pprint import pprint
 
-MONGO_URI = "mongodb://localhost:27017"
-client = MongoClient(MONGO_URI)
-db = client.admin
-dbs_list = db.command("listDatabases")
-pprint(dbs_list)
+print("Start")
+# MONGO_URI = "mongodb://localhost:27017"
+# client = MongoClient(MONGO_URI)
+# db = client.admin
+# dbs_list = db.command("listDatabases")
+# pprint(dbs_list)
+pprint("End")
